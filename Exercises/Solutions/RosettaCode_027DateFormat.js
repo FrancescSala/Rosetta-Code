e5354c667be1942e2e3ad39a60e3ef476927dfc1
FrancescSala <fsala.valcarcel@gmail.com>
2022-05-15 12:31:14 +0200

    exercises

---
+++ Exercises/Solutions/RosettaCode_027DateFormat.js
@@ -0,0 +1,12 @@
+function getDateFormats() {
+    const MONTHS = ['January','Febraury','March','April','May','June','July','August','September','October','November','December'];
+    const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let cd = new Date();     // today, the current date
+    let year = cd.getFullYear(); // year number
+    let month = cd.getMonth();   // month number, starting with 0 (0 is January)
+    let date = cd.getDate();     // day of the month
+    let day = cd.getDay();       // day of the week, starting with 0 (0 is Sunday)
+    return [year + '-' + (month+1) + '-' + date, DAYS[day] + ', ' + MONTHS[month] + ' ' + date + ', ' + year];
+  }
+
+console.log(getDateFormats());
