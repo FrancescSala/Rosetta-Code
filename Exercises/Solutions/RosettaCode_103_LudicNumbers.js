fd0acdfb0a9d555943d60501df7f63c8740d2223
FrancescSala <fsala.valcarcel@gmail.com>
2022-07-09 17:44:29 +0200

    exercises

--- Exercises/Solutions/RosettaCode_103_LudicNumbers.js
+++ Exercises/Solutions/RosettaCode_103_LudicNumbers.js
@@ -1,7 +1,6 @@
 function ludic(n) {
     if (isNaN(n) || n < 0 || Math.floor(n) !== n) throw Error('n should be a natural number');
-    if (n === 0) return [];
-    let L = [1];
+    let L = (n === 0) ? [] : [1];
     let candidates = Array.from(Array(n+1).keys()).slice(2); // [2,3,...,n]
     while (candidates.length !== 0) {
         let l = candidates[0]; 
@@ -11,10 +10,10 @@ function ludic(n) {
     return L;
 }
 
-/*
+console.log(ludic(0)); // should return [].
+console.log(ludic(1)); // should return [].
 console.log(ludic(2)); // should return [1, 2].
 console.log(ludic(3)); // should return [1, 2, 3].
 console.log(ludic(5)); // should return [1, 2, 3, 5].
 console.log(ludic(20)); // should return [1, 2, 3, 5, 7, 11, 13, 17].
 console.log(ludic(26)); // should return [1, 2, 3, 5, 7, 11, 13, 17, 23, 25].
-*/
---
+++ Exercises/Solutions/RosettaCode_104_LuhnTestOfCreditCardNumbers.js
@@ -0,0 +1,28 @@
+function luhnTest(str) {
+    if(typeof str != 'string') throw Error('str should be a String');
+    let digits = str.split('').reverse().map(elem => parseInt(elem));
+    if (digits.some(elem => isNaN(elem))) throw Error('str should contain numbers only');
+
+    let s1 = digits.reduce((accum,elem,index) => (index % 2 === 0) ? accum + elem : accum);
+    let s2 = digits.reduce((accum,elem,index) => {
+        if (index % 2 !== 0) {
+            let x = 2 * elem;
+            let s = 0;
+            while (x !== 0) {
+                s += x % 10;
+                x = Math.floor(x / 10);
+            }
+            accum += s;
+        }
+        return accum;
+    },0);
+    return ((s1 + s2) % 10 === 0);
+}
+
+
+console.log(luhnTest("4111111111111111")); // should return true.
+console.log(luhnTest("4111111111111112")); // should return false.
+console.log(luhnTest("49927398716")); // should return true.
+console.log(luhnTest("49927398717")); // should return false.
+console.log(luhnTest("1234567812345678")); // should return false.
+console.log(luhnTest("1234567812345670")); // should return true.
