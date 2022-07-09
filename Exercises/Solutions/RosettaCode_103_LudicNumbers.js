c24ca2e70721cee28238e4e91eefeeeddcf218a8
FrancescSala <fsala.valcarcel@gmail.com>
2022-07-09 16:54:53 +0200

    exercises

---
+++ Exercises/Solutions/RosettaCode_103_LudicNumbers.js
@@ -0,0 +1,20 @@
+function ludic(n) {
+    if (isNaN(n) || n < 0 || Math.floor(n) !== n) throw Error('n should be a natural number');
+    if (n === 0) return [];
+    let L = [1];
+    let candidates = Array.from(Array(n+1).keys()).slice(2); // [2,3,...,n]
+    while (candidates.length !== 0) {
+        let l = candidates[0]; 
+        L.push(l);
+        candidates = candidates.filter((elem,index) => index % l !== 0);
+    }
+    return L;
+}
+
+/*
+console.log(ludic(2)); // should return [1, 2].
+console.log(ludic(3)); // should return [1, 2, 3].
+console.log(ludic(5)); // should return [1, 2, 3, 5].
+console.log(ludic(20)); // should return [1, 2, 3, 5, 7, 11, 13, 17].
+console.log(ludic(26)); // should return [1, 2, 3, 5, 7, 11, 13, 17, 23, 25].
+*/
